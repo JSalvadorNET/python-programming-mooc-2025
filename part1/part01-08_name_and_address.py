4e29b9a64eb4f79cec50c996bf367f3406e0e29d
salvador <julyanaolacerda@gmail.com>
2025-01-25 17:42:53 -0300

    add

---
+++ part1/part01-08_name_and_address.py
@@ -0,0 +1,7 @@
+given_name = input("Given name:")
+family_name = input("Family name:")
+street_add = input ("Street address:")
+postal = input("City and postal code:")
+print(given_name +" "+ family_name)
+print(street_add)
+print(postal)
